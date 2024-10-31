4296deb87d187a4b740c16f8105f943a6436627f
Juan Valenzuela <juan_svalenzuela@soy.sena.edu.co>
2024-10-31 18:41:15 -0500

    Initial commit

---
+++ src/main/java/com/aluracursos/Back_fraseAleatoria/BackFraseAleatoriaApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursos.Back_fraseAleatoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackFraseAleatoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackFraseAleatoriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluracursos/Back_fraseAleatoria/BackFraseAleatoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.Back_fraseAleatoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackFraseAleatoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
